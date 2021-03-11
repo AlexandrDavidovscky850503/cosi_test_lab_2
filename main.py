0dedee92aa4c27b34e9e2cfa019dbee884c29309
Anton Kreis <antonkreis9@gmail.com>
2021-03-11 19:35:33 +0300

    Refactor

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 # This is a sample Python script.
 
-import numpy as myNumpy # общие математические и числовые операции
-import matplotlib.pyplot as plt # для построения графиков
+import numpy as myNumpy  # общие математические и числовые операции
+import matplotlib.pyplot as plt  # для построения графиков
 
 
 class FastFurieTransfsorm:
     mulCounter = 0
     addCounter = 0
-    @staticmethod # можно вызывать без создания экземпляра класса
-    def fastFurieTransform(self, function, direction): # self чтобы ссылаться на самих себя
+
+    @staticmethod  # можно вызывать без создания экземпляра класса
+    def fastFourierTransform(self, function, direction):  # self чтобы ссылаться на самих себя
         self.mulCounter = 0
         self.addCounter = 0
-        resultFunction = self.fastFurieTransformRecursive(self,function,direction)
+        resultFunction = self.fastFourierTransformRecursive(self, function, direction)
 
         if direction == 1:
             valuesLenght = len(function)
@@ -21,57 +22,56 @@ class FastFurieTransfsorm:
         return resultFunction
 
     @staticmethod  # можно вызывать без создания экземпляра класса
-    def fastFurieTransformRecursive(self, function, direction):
-        valuesLenght = len(function)
+    def fastFourierTransformRecursive(self, function, direction):
+        valuesLength = len(function)
 
-        if valuesLenght == 1:
+        if valuesLength == 1:
             return function
-        firstHalf = [complex(0, 0)] * (int(valuesLenght/2)) # Подготовка массивов для операции "Бабочка"
-        secondtHalf = [complex(0, 0)] * (int(valuesLenght / 2))
+        firstHalf = [complex(0, 0)] * (int(valuesLength / 2))  # Подготовка массивов для операции "Бабочка"
+        secondtHalf = [complex(0, 0)] * (int(valuesLength / 2))
 
-        Wn = complex(myNumpy.cos(2*myNumpy.pi/valuesLenght),direction * myNumpy.sin(2*myNumpy.pi/valuesLenght)) # Присвоить Wn значение главного комплексного корня N-й степени из единицы
-        w = 1 # Присвоить w = 1
-        result = [complex(0,0)]*valuesLenght
-        for i in range(int(valuesLenght/2)): # Операция "Бабочка"
-            firstHalf[i] = function[i] + function[i+int(valuesLenght/2)]
-            secondtHalf[i] = (function[i] - function[i+int(valuesLenght/2)])*w
-            w=w*Wn
+        # Присвоить Wn значение главного комплексного корня N-й степени из единицы
+        Wn = complex(myNumpy.cos(2*myNumpy.pi/valuesLength), direction * myNumpy.sin(2*myNumpy.pi/valuesLength))
+        w = 1  # Присвоить w = 1
+        result = [complex(0,0)]*valuesLength
+        for i in range(int(valuesLength/2)):  # Операция "Бабочка"
+            firstHalf[i] = function[i] + function[i + int(valuesLength / 2)]
+            secondtHalf[i] = (function[i] - function[i + int(valuesLength / 2)]) * w
+            w = w * Wn
             self.mulCounter += 1
             self.addCounter += 2
 
-        evenResult = self.fastFurieTransformRecursive(self,firstHalf,direction) # Рекурсивный вызов БПФ для каждой из частей
-        oddResult = self.fastFurieTransformRecursive(self,secondtHalf,direction)
-        for i in range(valuesLenght): # Объединение результатов
-            if i%2==0:
-                result[i] = evenResult[int(i/2)]
+        evenResult = \
+            self.fastFourierTransformRecursive(self, firstHalf, direction)  # Рекурсивный вызов БПФ для каждой из частей
+        oddResult = self.fastFourierTransformRecursive(self, secondtHalf, direction)
+        for i in range(valuesLength):  # Объединение результатов
+            if i % 2 == 0:
+                result[i] = evenResult[int(i / 2)]
             else:
-                result[i] = oddResult[int(i/2)]
+                result[i] = oddResult[int(i / 2)]
 
         return result
+
 
 class CorrelationWithConvolution:
     mulCounter = 0
     addCounter = 0
+
     @staticmethod
-    def correlationWithConvolutionOperation(firstFunction,secondFunction,operation):
+    def correlationWithConvolutionOperation(firstFunction, secondFunction, operation):
 
         length = len(firstFunction)
         CorrelationWithConvolution.mulCounter = 0
         CorrelationWithConvolution.addCounter = 0
-
-        # print("====")
 
         result = []
         for i in range(length):
             temp = 0
             for j in range(length):
                 k = myNumpy.abs(i + (j * operation)) % length
-                # k1 = (i + (j * operation)) % length
-                # print(k)
                 if (i + (j * operation)) < 0:
                     k = k * (-1)
 
-                # print(k)
                 temp += firstFunction[j] * secondFunction[k]
 
                 CorrelationWithConvolution.mulCounter += 1
@@ -90,27 +90,28 @@ class CorrelationWithConvolution:
         CorrelationWithConvolution.mulCounter = 0
         CorrelationWithConvolution.addCounter = 0
 
-        firstFunctionFastFurieTransform = FastFurieTransfsorm.fastFurieTransform(FastFurieTransfsorm, firstFunction, 1)
+        firstFunctionFastFourierTransform = \
+            FastFurieTransfsorm.fastFourierTransform(FastFurieTransfsorm, firstFunction, 1)
         CorrelationWithConvolution.mulCounter += FastFurieTransfsorm.mulCounter
         CorrelationWithConvolution.addCounter += FastFurieTransfsorm.addCounter
 
-        secondFunctionFastFurieTransform = FastFurieTransfsorm.fastFurieTransform(FastFurieTransfsorm, secondFuncton, 1)
+        secondFunctionFastFourierTransform = \
+            FastFurieTransfsorm.fastFourierTransform(FastFurieTransfsorm, secondFuncton, 1)
         CorrelationWithConvolution.mulCounter += FastFurieTransfsorm.mulCounter
         CorrelationWithConvolution.addCounter += FastFurieTransfsorm.addCounter
 
-        firstFunctionFastFurieTransformConjugate = firstFunctionFastFurieTransform
+        firstFunctionFastFourierTransformConjugate = firstFunctionFastFourierTransform
         if operation == 1:
-            firstFunctionFastFurieTransformConjugate = myNumpy.conj(firstFunctionFastFurieTransform)
+            firstFunctionFastFourierTransformConjugate = myNumpy.conj(firstFunctionFastFourierTransform)
 
-        temp = myNumpy.multiply(firstFunctionFastFurieTransformConjugate, secondFunctionFastFurieTransform)
+        temp = myNumpy.multiply(firstFunctionFastFourierTransformConjugate, secondFunctionFastFourierTransform)
         CorrelationWithConvolution.mulCounter += length
 
-        result = FastFurieTransfsorm.fastFurieTransform(FastFurieTransfsorm, temp, -1)
+        result = FastFurieTransfsorm.fastFourierTransform(FastFurieTransfsorm, temp, -1)
         CorrelationWithConvolution.mulCounter += FastFurieTransfsorm.mulCounter
         CorrelationWithConvolution.addCounter += FastFurieTransfsorm.addCounter
 
         return result
-
 
 
 # Press the green button in the gutter to run the script.
@@ -119,7 +120,6 @@ if __name__ == '__main__':
     arguments = myNumpy.arange(0, 16) * 2 * myNumpy.pi / N
     firstFunction = list(map(lambda x: myNumpy.cos(x), arguments))
     secondFunction = list(map(lambda x: myNumpy.sin(3 * x), arguments))
-   # print('    : {}'.format(arguments))
 
     correlation = CorrelationWithConvolution.correlationWithConvolutionOperation(firstFunction, secondFunction, 1)
     print('Сложность корреляции (умножение): {}'.format(CorrelationWithConvolution.mulCounter))
@@ -128,10 +128,12 @@ if __name__ == '__main__':
     print('Сложность свертки (умножение): {}'.format(CorrelationWithConvolution.mulCounter))
     print('Сложность свертки (сложение): {}'.format(CorrelationWithConvolution.addCounter))
 
-    correlationWithFastFurieTransform = CorrelationWithConvolution.FFTCorrelationWithConvolutionOperation(firstFunction, secondFunction, 1)
+    correlationWithFastFourierTransform = \
+        CorrelationWithConvolution.FFTCorrelationWithConvolutionOperation(firstFunction, secondFunction, 1)
     print('Сложность корреляции c БПФ (умножение): {}'.format(CorrelationWithConvolution.mulCounter))
     print('Сложность корреляции c БПФ (сложение): {}'.format(CorrelationWithConvolution.addCounter))
-    convolutionWithFastFurieTransform = CorrelationWithConvolution.FFTCorrelationWithConvolutionOperation(firstFunction, secondFunction, -1)
+    convolutionWithFastFourierTransform = \
+        CorrelationWithConvolution.FFTCorrelationWithConvolutionOperation(firstFunction, secondFunction, -1)
     print('Сложность свертки c БПФ (умножение): {}'.format(CorrelationWithConvolution.mulCounter))
     print('Сложность свертки c БПФ (сложение): {}'.format(CorrelationWithConvolution.addCounter))
 
@@ -163,17 +165,14 @@ if __name__ == '__main__':
     ax_3.scatter(arguments, convolution, color='orange')
     ax_3.grid(True)
 
-    ax_5.plot(arguments, correlationWithFastFurieTransform)
+    ax_5.plot(arguments, correlationWithFastFourierTransform)
     ax_5.set(title='Корреляция с БПФ')
-    ax_5.scatter(arguments, correlationWithFastFurieTransform, color='orange')
+    ax_5.scatter(arguments, correlationWithFastFourierTransform, color='orange')
     ax_5.grid(True)
 
-    ax_6.plot(arguments, convolutionWithFastFurieTransform)
+    ax_6.plot(arguments, convolutionWithFastFourierTransform)
     ax_6.set(title='Свертка с БПФ')
-    ax_6.scatter(arguments, convolutionWithFastFurieTransform, color='orange')
+    ax_6.scatter(arguments, convolutionWithFastFourierTransform, color='orange')
     ax_6.grid(True)
 
     plt.show()
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
