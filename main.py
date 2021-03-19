838cdf53671489bfb42573e3006642009d972a18
Anton Kreis <antonkreis9@gmail.com>
2021-03-19 11:03:10 +0300

    Code improvements

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt  # для построения графиков
 import sympy as sm
 
 
-def fwht_new(function):
+def fwht(function):
     result = fwht_rec(function)
 
     values_length = len(function)
@@ -22,7 +22,6 @@ def fwht_rec(function):
         return function
     first_half = []  # Подготовка массивов для операции "Бабочка"
     second_half = []
-    result = []
 
     for i in range(int(values_length / 2)):  # Операция "Бабочка"
         first_half.append(function[i] + function[i + int(values_length / 2)])
@@ -40,31 +39,30 @@ def fwht_rec(function):
 if __name__ == '__main__':
     N = 16
     arguments = np.arange(0, N) * 2 * np.pi / N
-    function = list(map(lambda x: np.sin(3*x) + np.cos(x), arguments))
-    # result = fwht(function)
-    result = fwht_new(function)
-    print(result)
-    res = sm.fwht(function)
-    print(res)
+    function = list(map(lambda x: np.sin(3 * x) + np.cos(x), arguments))
+    result = fwht(function)
+    print(f'fwht: {result}')
+    sympy_res = sm.fwht(function)
+    print(f'sympy.fwht: {sympy_res}')
 
     fig = plt.figure()
     ax_1 = fig.add_subplot(1, 3, 1)
-    ax_4 = fig.add_subplot(1, 3, 2)
-    ax_5 = fig.add_subplot(1, 3, 3)
+    ax_2 = fig.add_subplot(1, 3, 2)
+    ax_3 = fig.add_subplot(1, 3, 3)
 
     ax_1.plot(arguments, function)
-    ax_1.set(title='sin(3x)+cos(x)')
+    ax_1.set(title='sin(3x) + cos(x)')
     ax_1.scatter(arguments, function, color='orange')
     ax_1.grid(True)
 
-    ax_4.plot(arguments, result)
-    ax_4.set(title='fwht')
-    ax_4.scatter(arguments, result, color='orange')
-    ax_4.grid(True)
+    ax_2.plot(arguments, result)
+    ax_2.set(title='fwht')
+    ax_2.scatter(arguments, result, color='orange')
+    ax_2.grid(True)
 
-    ax_5.plot(arguments, res)
-    ax_5.set(title='sympy.fwht')
-    ax_5.scatter(arguments, res, color='orange')
-    ax_5.grid(True)
+    ax_3.plot(arguments, sympy_res)
+    ax_3.set(title='sympy.fwht')
+    ax_3.scatter(arguments, sympy_res, color='orange')
+    ax_3.grid(True)
 
     plt.show()
